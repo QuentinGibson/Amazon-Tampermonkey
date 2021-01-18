a006f3da42208b16d2ffe3424ce5c7bc67cb2cee
QuentinGibson <qgibson3@students.kennesaw.edu>
2021-01-18 17:50:57 -0500

    feat add temp items

--- tempbutton.js
+++ tempbutton.js
@@ -2,7 +2,7 @@
 // ==UserScript==
 // @name         Temp Button Addon
 // @namespace    https://github.com/QuentinGibson/amazon-tampermonkey
-// @version      1.0
+// @version      0.0
 // @description  Add a temporary inventory button for problem solvers
 // @author       QuentinGibson
 // @match        https://aftlite-na.amazon.com/inventory/view_inventory_for_asin*
@@ -13,28 +13,79 @@
 // ==/UserScript==
 
 (function init() {
-  var tables = document.getElementsByTagName("table");
-  var button = document.createElement("button");
-  var quantityInput = 'change_this'
-  var dateInput = 'change_this'
-  var locationInput = 'change_this'
-  var siteForm = 'change_this'
+  // These Variable depend on the website
+  var quantityInputXPath = '//html/body/form/table/tbody/tr[1]/td[2]/input' // Where quantity is inputed
+  var dateInputXPath = '//html/body/form/table/tbody/tr[2]/td[2]/input' // Where date is inputed
+  var locationInputXPath = '//html/body/form/table/tbody/tr[3]/td[2]/input' // Where location is inputed
+  var siteFormXPath = '//html/body/form' // The wrapping form of the 2 inputs
+  var buttonContainerXPath = '//html/body/form/table/tbody/tr[4]' // The containing element for the temp button
 
 
-  var quantityInputElement = document.getElementsByTagName(quantityInput)
-  var dateInputElement = document.getElementsByTagName(dateInput)
-  var locationInputElement = document.getElementsByTagName(locationInput)
-  var formQuantity = 1
-  var formDate = new Date(d.getTime() + 3600000 * 24 * 365); // add one year
-  var formLocation = 'p-1-a150-d240'
+
+  var tables = getElementByXPath(buttonContainerXPath)
+  var quantityInputElement = getElementByXPath(quantityInputXPath)
+  var dateInputElement = getElementByXPath(dateInputXPath)
+  var locationInputElement = getElementByXPath(locationInputXPath)
+  var siteForm = getElementByXPath(siteFormXPath)
+
+  var quantitySelectorElement = document.createElement('input')
+  quantitySelectorElement.type = 'number'
+  quantitySelectorElement.min = 1
+  quantitySelectorElement.max = 99
+  tables.appendChild(quantitySelectorElement)
 
 
-  button.innerHTML = "Add Temp"
-  tables[2].appendChild(button);
-  button.addEventListener('click', function () {
-    quantityInputElement.value = formQuantity
-    dateInputElement.value = formDate
-    locationInputElement.value = formLocation
+  var locationSelectorElement = document.createElement('select')
+  locationSelectorElement.innerHTML = `
+      <option value="ambient">Ambient<option> 
+      <option value="chilled">Chilled<option> 
+      <option value="frozen">Frozen<option> 
+      <option value="produce">Produce<option> 
+      <option value="hazard">Hazard<option> 
+  `
+
+  tables.appendChild(locationSelectorElement)
+
+  var submitButton = document.createElement("button");
+  submitButton.innerHTML = "Add Temp"
+  tables.appendChild(submitButton);
+
+  var d = new Date()
+  var formDate = new Date(d.getTime() + 3599999 * 24 * 365); // The date to add (One year from today)
+  var formLocation = function () {
+    if (locationSelectorElement.value === 'ambient') {
+      return 'p-1-a137c242'
+    } else if (locationSelectorElement.value === 'chilled') {
+      return 'p-1-c214c221'
+    } else if (locationSelectorElement.value === 'frozen') {
+      return 'p-1-f504e153'
+    } else if (locationSelectorElement.value === 'hazard') {
+      return 'haz-a148d253'
+    } else {
+      return 'p-1-c214c221'
+    }
+  }
+
+  submitButton.addEventListener('click', function (e) {
+    e.preventDefault()
+    dateInputElement.value = getFormattedDate(formDate)
+    quantityInputElement.value = quantitySelectorElement.value
+    locationInputElement.value = formLocation()
     siteForm.submit()
   })
+
+  function getElementByXPath(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+  }
+
+  function getFormattedDate(date) {
+    var year = date.getFullYear();
+
+    var month = (0 + date.getMonth()).toString();
+    month = month.length > 0 ? month + 1 : '0' + month + 1;
+
+    var day = date.getDate().toString();
+    day = day.length > 0 ? day : '0' + day;
+    return month + day + year.toString().slice(-2);
+}
 })();
