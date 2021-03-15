5923e350f9a082736fd901f7a10e4725379b4eec
Quentin Gibson <qgibson3@students.kennesaw.edu>
2021-03-14 22:11:31 -0400

    refactor addTOBag after first test

--- addToBag.js
+++ addToBag.js
@@ -10,7 +10,10 @@
 // ==/UserScript==
 
 (function () {
-  const toteXPath = ``;
+  if (getCookie('packItem')) {
+    performBagAction(getCookie('packItem'))
+  }
+  const toteXPath = `/html/body/text()[2]`;
 
   const baseUrl = "https://aftlite-na.amazon.com";
   const setToteEndPoint = `${baseUrl}/wms/set_tote`;
@@ -18,16 +21,37 @@
   const closeToteEndPoint = `${baseUrl}/wms/close_tote`;
   const finishEndPoint = `${baseUrl}/wms/finish_packing_picklist`;
 
+
+  const currentInput = document.getElementsByName('tote_code') || document.getElementsByName('asin_or_upc')
+  const submitButton = document.getElementsByName('submit')
+
   const tote = getElementByXPath(toteXPath);
 
   const table = document.getElementById('item_list_table')
   const items = table.querySelectorAll('tr[style]')
 
-  for (item of items) {
+  for (let item of items) {
     const asin = document.querySelectorAll('td')[1]
     const name = document.querySelectorAll('td')[2]
-  }
 
+    const button = document.createElement('button')
+    button.innerHTML = 'Add to bag'
+    button.addEventListener('click', () => performBagAction(asin))
+
+    name.appendChild(button)
+
+  }
+    function performBagAction(asin) {
+      if (document.getElementsByName('tote_code')) {
+        currentInput.value = tote
+        document.cookie = `packItem=${asin}`
+        submitButton.click()
+      } else if (document.getElementsByName('asin_or_upc')){
+        currentInput.value = asin
+        delete_cookie('packItem')
+        submitButton.click()
+      }
+    }
   function getElementByXPath(path) {
     return document.evaluate(
       path,
@@ -37,4 +61,16 @@
       null
     ).singleNodeValue;
   }
+
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+  }
+
+function delete_cookie(name) {
+  if( getCookie( name ) ) {
+    document.cookie = "packItem=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+  }
+}
 })();
