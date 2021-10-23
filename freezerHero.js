cf4191cb812ab5384ac8eb007026ae94c1f9f604
Quentin Gibson <qgibson3@students.kennesaw.edu>
2021-10-23 18:46:24 -0400

    add freezer

---
+++ freezerHero.js
@@ -0,0 +1,58 @@
+// ==UserScript==
+// @name         FreezerSurveyHelper
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *gsflearning.sjc1.qualtrics.com/jfe/form/*
+// @icon         https://www.google.com/s2/favicons?domain=qualtrics.com
+// @require http://code.jquery.com/jquery-3.4.1.min.js
+// @grant        none
+// run_at        document-end
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    jQuery.noConflict();
+    jQuery(document).ready(function ($) {
+        setTimeout(setInterval(start, 1000), 2000)
+       
+        function start() {
+             const answerQuestion = questionClassName => {
+            if (questionClassName !== null) { jQuery(`#${questionClassName}`).click() }
+            jQuery('#NextButton').click()
+        }
+            const UGA2 = 107
+
+            if (isFirstPage()) { jQuery('.Selection')[0].value = UGA2; jQuery('img').hide() }
+            if (isSecondPage()) { answerQuestion('QID8-2-label') }
+            if (isThirdPage()) { answerQuestion('QID5-1-label') }
+            if (isFourthPage()) { answerQuestion('QID6-1-label') }
+            if (isLastPage()) { answerQuestion(null) }
+
+            function isFirstPage() {
+                return jQuery('.QuestionText')[0].innerHTML === 'What is your login?'
+
+            }
+            function isSecondPage() {
+                return jQuery('.QuestionText')[0].innerHTML === "Was any piece of PPE unavailable?"
+            }
+            function isThirdPage() {
+                if (jQuery('.QuestionText div span span')[0]) {
+                    return jQuery('.QuestionText div span span')[0].innerHTML === "You have a 2 hour max time per 24 hours, <br>"
+                }
+            }
+            function isFourthPage() {
+                if (jQuery('.QuestionText div span')[0]) {
+                    return jQuery('.QuestionText div span')[0].innerHTML === "You <strong>must</strong> warm up every 30 minutes for 5 minutes. <br>"
+                }
+            }
+            function isLastPage() {
+                if (jQuery('.QuestionText div strong span')[0]) {
+                    return jQuery('.QuestionText div strong span')[0].innerHTML ===  "You may enter the freezer safely!"
+                }
+            }
+        }
+
+    });
+})();
