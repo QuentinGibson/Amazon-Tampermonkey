fa20a8c3aa2fa25e9ba5c8387724bebf5f5fae3a
Quentin Gibson <qgibson3@students.kennesaw.edu>
2021-12-04 19:05:17 -0500

    correct includes from pages

--- freezerSurvey.js
+++ freezerSurvey.js
@@ -18,8 +18,8 @@
         const firstPage = jQuery('.QuestionText')[0].innerHTML.includes('What is your login?')
         const secondPage = jQuery('.QuestionText')[0].innerHTML.includes('Was any piece of PPE unavailable?')
         const thirdPage = jQuery('.QuestionText')[0].innerHTML.includes('You have a 2 hour max time per 24 hours,')
-        const fourthPage = jQuery('.QuestionText')[0].innerHTML.includes('<div style="text-align: center;"><span style="font-size: 29px; outline: rgb(0, 0, 0) solid 0px;" tabindex="-1">You <strong>must</strong> warm up every 30 minutes for 5 minutes. <br></span></div><div style="text-align: center;"><span style="font-size:29px;">Do you commit to that warm up period?</span><br>\n<img src="https://gsflearning.sjc1.qualtrics.com/CP/Graphic.php?IM=IM_6htHVD5abJmS84u" style="width: 526px; height: 499px;" data-image-state="ready"></div>')
-        const lastPage = jQuery('.QuestionText')[0].innerHTML.includes('<div style="text-align: center;"><strong><span style="font-size: 32px; outline: rgb(0, 0, 0) solid 0px;" tabindex="-1">You may enter the freezer safely!</span></strong></div>')
+        const fourthPage = jQuery('.QuestionText')[0].innerHTML.includes('warm up every 30 minutes for 5 minutes.')
+        const lastPage = jQuery('.QuestionText')[0].innerHTML.includes('You may enter the freezer safely!')
 
         const submitPage = () => {
             jQuery('#NextButton').click()
